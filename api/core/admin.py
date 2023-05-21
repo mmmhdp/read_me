644c4d08098712dc0a2d2206aa883f217b5668f7
Malcev Nikita <malcevnik99@gmail.com>
2023-05-21 21:58:46 +0300

    create models and add them to admin site

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from core.models import Note, Author, SubGenre, PubInfo, Book
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Note)
+admin.site.register(PubInfo)
+admin.site.register(SubGenre)
--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+GLOBAL_GENRES = [('fiction','non-fiction')]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length = 200)
+    second_n = models.CharField(max_length = 200)
+    last_n = models.CharField(blank = True,
+                              max_length = 200)
+    alias = models.CharField(blank = True, 
+                             max_length = 200)
+
+
+class SubGenre(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 600, blank = True)
+
+
+class PubInfo(models.Model):
+    publisher = models.CharField(max_length = 200)
+    year = models.IntegerField(blank = True, default = 0)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length = 400)
+    summary = models.TextField()
+    global_genre = models.CharField(choices = GLOBAL_GENRES,
+                                    default = "written",
+                                    max_length = 100)
+    
+    sub_genre = models.ForeignKey(SubGenre,
+                                  on_delete = models.SET("no info"),
+                                  blank = True,
+                                  related_name = "book")
+    pub_info = models.ForeignKey(PubInfo,
+                                 on_delete = models.SET("no info"),
+                                 blank = True,
+                                 related_name = "book")
+
+    author = models.ManyToManyField(Author, related_name = "book")
+  
+
+
+class Note(models.Model):
+    related_paper = models.OneToOneField(Book,
+                                         on_delete = models.CASCADE,
+                                         related_name = "note")
+    review = models.TextField(blank = True)
+    rate = models.IntegerField(blank = True, default = 0)
+    created = models.DateTimeField(auto_now_add = True)
+ 
