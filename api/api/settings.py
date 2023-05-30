ee973706d3e4b7105fa152c400b3b85c22510731
Malcev Nikita <malcevnik99@gmail.com>
2023-05-31 00:06:03 +0300

    version 0.1

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core',
     'drf_yasg',
     'django_extensions',
     'rest_registration',
+    'core',
 ]
 
 MIDDLEWARE = [
